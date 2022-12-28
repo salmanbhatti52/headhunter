c316609d3709bfcff2988a293e40cf9fe67dd7c5
Salman Ahmad Bhatti <salmanbhatti52@hotmail.com>
2022-12-28 17:01:57 +0500

    Categories update
    Categories update

--- platforms/android/app/src/main/assets/www/polyfills-es2015.js
+++ platforms/android/app/src/main/assets/www/polyfills-es2015.js
@@ -6514,8 +6514,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/salmanahmad/Desktop/head/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/salmanahmad/Desktop/head/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/salmanahmad/Desktop/headhunter/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/salmanahmad/Desktop/headhunter/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
